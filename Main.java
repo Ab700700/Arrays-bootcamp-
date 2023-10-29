8bad0f92507b5771e41064be0779b6c05ceffa60
Ab700700 <148686321+Ab700700@users.noreply.github.com>
2023-10-29 18:24:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+            //1.Write a Java program to test if the first and the last element of an array of
+        //integers are same. The length of the array must be greater than or equal to
+        //2
+
+//        int ar[] ={50,-20,0,30,40,60,10};
+//        System.out.println(ar[0]==ar[ar.length-1]);
+
+        //2.Write a Java program to find the numbers greater than the average of the
+        //numbers of a given array.
+
+//        int ar[] = {1,4,17,7,25,3,100};
+//        int sum = 0 ;
+//        for(int n : ar) sum+=n;
+//        double av = sum/ar.length;
+//        ArrayList<Integer> arr = new ArrayList<>();
+//        for(int i =0 ; i<ar.length;i++){
+//            if(ar[i]>av) arr.add(ar[i]);
+//        }
+//        System.out.printf("The average of the said array is: %f \n",av);
+//        System.out.print("The numbers in the said array that are greater than the average are: "+arr);
+
+        // 3.Write a Java program to get the larger value between first and last
+        //element of an array of integers.
+
+//        int ar[] = {20,30,40};
+//
+//        int max = ar[0] ;
+//        for(int i = 1 ;i<ar.length;i++) if(ar[i]>max) max = ar[i];
+//        System.out.printf("Lager value between first and last element: %d",max);
+
+        //4.Write a Java program to swap the first and last elements of an array and
+        //create a new array.
+
+//        int ar [] = {20,30,40};
+//        int temp = ar[0];
+//        ar[0]= ar[ar.length-1];
+//        ar[ar.length-1] = temp;
+//
+//        System.out.println("New array after swapping the first and last elements: "+ Arrays.toString(ar));
+
+        //5. Write a program that places the odd elements of an array before the
+        //even elements.
+
+//        int ar[] = {2,3,40,1,5,9,4,10,7};
+//
+//        ArrayList<Integer> newAr = new ArrayList<>();
+//        for(int i = 0; i<ar.length;i++){
+//            if(ar[i]%2!=0) newAr.add(ar[i]);
+//        }
+//        for(int i = 0; i<ar.length;i++){
+//            if(ar[i]%2==0) newAr.add(ar[i]);
+//        }
+//        System.out.println(newAr);
+
+
+        //6. Write a program that test the equality of two arrays.
+
+//        int [] ar1 = {2,3,6,6,4};
+//        int [] ar2 = {2,3,6,6,4};
+//        if(ar2.length == ar1.length) {
+//            boolean [] ar = new boolean[ar1.length];
+//           for(int n : ar1){
+//               for(int i = 0 ; i<ar1.length;i++){
+//                   if(n == ar2[i]) {
+//                       ar2[i]=0;
+//                       ar[i]=true;
+//                       break;
+//                   }
+//               }
+//           }
+//            boolean res = true;
+//            for(boolean a : ar) {if(!a) res=false;}
+//            System.out.println(res);
+//        }else{
+//            System.out.println(false);
+//        }
+
+
+        }
+    }
